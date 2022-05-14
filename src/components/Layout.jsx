8a83dfa8e5b760ae0b669fec2e4aa5642f7c6494
jisena.btc <pjohmaths@gmail.com>
2022-05-14 18:34:36 +0300

    added dummy.js

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { para } from './dummy';
 
 export class Layout extends Component {
     constructor(props) {
@@ -11,7 +12,25 @@ export class Layout extends Component {
 
   render() {
     return (
-      <div>Layout</div>
+      <div className='grid grid-cols-4 gap-2'>
+        <header className='col-span-4 border-b-2 border-orange-400 bg-slate-400'>
+          {para}
+        </header>
+        <section className='col-span-3 border-t-2 border-b-2 border-orange-400 bg-slate-400 rounded'>
+          <p>
+            {para}
+          </p>
+          <p>
+            {para}
+          </p>
+          <p>
+            {para}
+          </p>
+        </section>
+        <p className='border-t-2 border-b-2 border-orange-400 bg-slate-400 rounded'>
+          {para}
+        </p>
+      </div>
     )
   }
 }
