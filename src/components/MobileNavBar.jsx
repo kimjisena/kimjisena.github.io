4f54f059109961f14574e2e64e12ce59de2e547a
jisena.btc <pjohmaths@gmail.com>
2022-05-15 11:07:00 +0300

    decoupled MobileNavBar.jsx

--- src/components/MobileNavBar.jsx
+++ src/components/MobileNavBar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { FaBars } from 'react-icons/fa';
 import MobileSideNav from './MobileSideNav';
+import MobileTopNav from './MobileTopNav';
 
 export default class MobileNavBar extends Component {
     constructor(props) {
@@ -21,14 +21,7 @@ export default class MobileNavBar extends Component {
   render() {
     return (
       <>
-        <div className='col-span-2' onClick={this.showSideNav}>
-            <FaBars className='w-8 h-8 mx-auto pt-1 text-cool-white'/>
-        </div>
-        <h1 className='col-start-4 col-span-8 text-center 
-                       text-2xl font-bold font-my-sans 
-                       pt-1 align-middle text-cool-white'>
-            {'<jisena />'}
-        </h1>
+        <MobileTopNav />
         <MobileSideNav show={this.state.side} />
       </>
     )
---
+++ src/components/MobileTopNav.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { FaBars } from 'react-icons/fa';
+
+export default class MobileTopNav extends Component {
+  render() {
+    return (
+      <>
+        <div className='col-span-2' onClick={this.showSideNav}>
+            <FaBars className='w-8 h-8 mx-auto pt-1 text-cool-white'/>
+        </div>
+        <h1 className='col-start-4 col-span-8 text-center 
+                       text-2xl font-bold font-my-sans 
+                       pt-1 align-middle text-cool-white'>
+            {'<jisena />'}
+        </h1>
+      </>
+    )
+  }
+}
