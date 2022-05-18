ff57ab581bc517139f8929870f89f651d21d3f0e
jisena.btc <pjohmaths@gmail.com>
2022-05-18 23:14:22 +0300

    edit up arrow

--- src/components/mobile/BackToTheTop.jsx
+++ src/components/mobile/BackToTheTop.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { FaChevronUp } from 'react-icons/fa';
+import {BiUpArrowAlt} from 'react-icons/bi';
 
 export default function BackToTheTop() {
   return (
     <div className='fixed right-2 top-[90%] rounded-full bg-cool-blue border-[1px] h-12 w-12'>
-      <FaChevronUp className='w-8 h-8 text-cool-white mx-auto mt-2'/>
+      <BiUpArrowAlt className='w-8 h-8 text-cool-white mx-auto mt-2'/>
     </div>
   );
 }
