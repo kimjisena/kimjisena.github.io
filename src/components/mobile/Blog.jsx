5df69c85c48e3731e6b6f931b0a27f78ef1f2c22
jisena.btc <pjohmaths@gmail.com>
2022-05-18 13:34:45 +0300

    apply website theme

--- src/components/mobile/Blog.jsx
+++ src/components/mobile/Blog.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default function Blog() {
   return (
     <section id='blog' className='col-span-full'>
-      Blog
-      <p>
+      <h1 className='text-4xl text-cooler-black font-bold border-b-2 border-cooler-blue'>my blog</h1>
+      <p className='text-cooler-black text-lg'>
         Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
       </p>
-      <p>
+      <p className='text-cooler-black text-lg'>
         Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
       </p>
-      <p>
+      <p className='text-cooler-black text-lg'>
         Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
       </p>
     </section>
