5b8da018b09df29031755fd0dadfcc968627314d
jisena.btc <pjohmaths@gmail.com>
2022-05-18 12:37:37 +0300

    fixed background color

--- src/components/mobile/About.jsx
+++ src/components/mobile/About.jsx
@@ -22,18 +22,26 @@ export default function About() {
             Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
           </p>
         <h1>what do i</h1>
+        <p>
+            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
+          </p>
+          <p>
+            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
+          </p>
+          <p>
+            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
+          </p>
         <h1>what love i</h1>
+        <p>
+            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
+          </p>
+          <p>
+            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
+          </p>
+          <p>
+            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
+          </p>
       </div>
-
-      {/* <p>
-        Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
-      </p>
-      <p>
-        Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
-      </p>
-      <p>
-        Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
-      </p> */}
     </section>
   );
 }
--- src/components/mobile/MobileMain.jsx
+++ src/components/mobile/MobileMain.jsx
@@ -6,7 +6,7 @@ import Contact from './Contact';
 
 export default function MobileMain() {
   return (
-    <div className='bg-red-300 px-2 mt-12 grid grid-cols-12'>
+    <div className='px-2 mt-12 grid grid-cols-12'>
         <About />
         <Projects />
         <Blog />
