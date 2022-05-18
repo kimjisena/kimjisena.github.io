f4a927d55157226dffe4c8a59d8e57a9fae5a8a7
jisena.btc <pjohmaths@gmail.com>
2022-05-18 09:28:22 +0300

    main mobile layout

--- src/components/mobile/Mobile.jsx
+++ src/components/mobile/Mobile.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import TopNav from './TopNav';
+import SideNav from './SideNav';
+import Main from './Main';
 
 export default function Mobile() {
   return (
-    <div className='md:hidden'>Mobile</div>
+    <div className='md:hidden'>
+        Mobile
+        <TopNav />
+        <SideNav />
+        <Main />
+    </div>
   );
 }
