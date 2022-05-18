f7d8e2a9c79ba76afcf69239a134ecba8214a5ec
jisena.btc <pjohmaths@gmail.com>
2022-05-18 13:25:51 +0300

    style about section

--- src/components/mobile/About.jsx
+++ src/components/mobile/About.jsx
@@ -1,44 +1,49 @@
 import React from 'react';
-import logo1 from '../../res/images/logo1.png';
+//import logo1 from '../../res/images/logo1.png';
+import logo2 from '../../res/images/logo2.png';
 
 export default function About() {
   return (
     <section id='about' className='col-span-full grid grid-cols-3'>
+      {/* image and text logo */}
       <div className='col-span-1 col-start-2'>
-        <div className='rounded-full bg-cool-green mt-1'>
-          <img src={logo1} alt='' />
+        <div className='rounded-full bg-cool-black mt-1'>
+          <img src={logo2} alt='' />
         </div>
-        <h1 className='text-center text-cool-green font-bold font-my-sans text-2xl'>{'<jisena />'}</h1>
+        <h1 className='text-center text-cool-black font-bold font-my-sans text-2xl'>{'<jisena />'}</h1>
       </div>
+
+      {/*section main content*/}
       <div className='col-span-full mt-1'>
-        <h1>who am i</h1>
-          <p>
+        <h1 className='text-4xl text-cooler-black font-bold border-b-2 border-cooler-blue'>about me</h1>
+        <h2 className='text-cooler-blue text-2xl font-bold'>who am i</h2>
+          <p className='text-cooler-black text-lg'>
             Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
           </p>
-          <p>
+          <p className='text-cooler-black text-lg'>
             Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
           </p>
-          <p>
+          <p className='text-cooler-black text-lg'>
             Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
           </p>
-        <h1>what do i</h1>
-        <p>
+        <h2 className='text-cooler-blue text-3xl font-bold'>what do i</h2>
+        <p className='text-cooler-black text-lg'>
             Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
           </p>
-          <p>
+          <p className='text-cooler-black text-lg'>
             Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
           </p>
-          <p>
+          <p className='text-cooler-black text-lg'>
             Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
           </p>
-        <h1>what love i</h1>
-        <p>
+        <h2 className='text-cooler-blue text-3xl font-bold'>what love i</h2>
+        <p className='text-cooler-black text-lg'>
             Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
           </p>
-          <p>
+          <p className='text-cooler-black text-lg'>
             Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
           </p>
-          <p>
+          <p className='text-cooler-black text-lg'>
             Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
           </p>
       </div>
