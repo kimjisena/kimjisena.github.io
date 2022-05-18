44a8089d11d0639f3222b35ffb60628776bf7d58
jisena.btc <pjohmaths@gmail.com>
2022-05-18 12:46:54 +0300

    color text logo and center-align

--- src/components/mobile/About.jsx
+++ src/components/mobile/About.jsx
@@ -4,11 +4,11 @@ import logo1 from '../../res/images/logo1.png';
 export default function About() {
   return (
     <section id='about' className='col-span-full grid grid-cols-3'>
-      <div className='col-span-1 col-start-2 bg-orange-900'>
+      <div className='col-span-1 col-start-2'>
         <div className='rounded-full bg-cool-green mt-1'>
           <img src={logo1} alt='' />
         </div>
-        <h1 className='text-center'>{'<jisena />'}</h1>
+        <h1 className='text-center text-cool-green font-bold font-my-sans text-2xl'>{'<jisena />'}</h1>
       </div>
       <div className='col-span-full mt-1'>
         <h1>who am i</h1>
