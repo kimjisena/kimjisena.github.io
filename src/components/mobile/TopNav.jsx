5e874f043fa4653b102475033618e83d0f3002d7
jisena.btc <pjohmaths@gmail.com>
2022-05-18 21:59:01 +0300

    edit header, looking promising

--- src/components/mobile/TopNav.jsx
+++ src/components/mobile/TopNav.jsx
@@ -11,15 +11,17 @@ export default function TopNav() {
       <div className='grid grid-cols-3 h-full'>
       <div className='border-[1px] border-cool-black col-span-1 w-20 mt-3 ml-2 flex flex-row justify-between h-9 rounded-full bg-cool-black'>
         <div className='order-2'>
-          <MdLightMode className='text-[#f5af8c]' size='32px' />
+          <MdLightMode className='text-cool-white' size='32px' />
         </div>
         <div className='order-1'>
-          <MdDarkMode className='text-cool-white' size='32px' />
+          <MdDarkMode className='text-[#c0876a]' size='32px' />
         </div>
       </div>
       <h1 className='text-center text-cool-white font-bold font-my-sans text-2xl align-middle mt-3 col-span-1'>{'<jisena />'}</h1>
-      <div className='mt-3 col-span-1'>
-        <VscMenu className='text-cool-white' size='32px'/>
+      <div className='col-span-1 flex flex-row-reverse pr-4'>
+        <div className='mt-3'>
+          <VscMenu className='text-cool-white' size='32px'/>
+        </div>
       </div>
       </div>
 
