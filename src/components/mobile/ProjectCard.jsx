d1737dacd0d9b06933f8e25942e3ea470a64f327
jisena.btc <pjohmaths@gmail.com>
2022-05-19 18:47:01 +0300

    projects section responsive to theme toggle

--- src/components/mobile/ProjectCard.jsx
+++ src/components/mobile/ProjectCard.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useContext} from 'react';
 import logo1 from '../../res/images/logo1.png';
+import { ThemeContext } from '../theme-context';
 
 export default function ProjectCard() {
+  const theme = useContext(ThemeContext);
   return (
-    <div className='flex flex-row border-2 border-cool-blue rounded-lg border-dotted mb-3'>
+    <div className={`flex flex-row border-2 ${theme.H1} rounded-lg border-dotted mb-3`}>
         {/*project logo*/}
-        <div className='w-20 h-20 shrink-0 self-center border-r-2 border-cool-blue border-dotted'>
+        <div className={`w-20 h-20 shrink-0 self-center border-r-2 ${theme.H1} border-dotted`}>
             <img src={logo1} alt=''/>
         </div>
 
         {/*project title and description*/}
         <div className='ml-2'>
-            <h2 className='text-cooler-black text-2xl font-bold mt-1 font-my-sans'>shit racer</h2>
-            <p className='text-cooler-black text-lg font-my-sans'>
+            <h2 className={`${theme.pH2} text-2xl font-bold mt-1 font-my-sans`}>shit racer</h2>
+            <p className={`${theme.pH2} text-lg font-my-sans`}>
                 Ipsum est proident et ea non.
                 Officia ut magna in deserunt consequat laborum
-                {/*project link*/} <a href={'#about'} className='text-cool-blue hover:text-cool-green visited:text-cool-yellow underline'>fugiat sint</a>.
+                {/*project link*/} <a href={'#about'} className={`${theme.H1} hover:text-cool-green visited:text-cool-yellow underline`}>fugiat sint</a>.
             </p>
         </div>
     </div>
--- src/components/mobile/Projects.jsx
+++ src/components/mobile/Projects.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import ProjectCard from './ProjectCard';
+import { ThemeContext } from '../theme-context';
 
 export default function Projects() {
+  const theme = useContext(ThemeContext);
+
   return (
     <section className='col-span-full mt-2'>
       <h1 id='projects' className='invisible h-12'>my projects</h1>
-      <h1 className='text-4xl text-cool-blue font-bold border-b-2 border-cool-black font-my-sans'>my projects</h1>
+      <h1 className={`text-4xl ${theme.H1} font-bold border-b-2 ${theme.h1Border} font-my-sans`}>my projects</h1>
       {/*introductory text*/}
-      <p className='text-cooler-black text-lg mb-3 font-my-sans'>
+      <p className={`${theme.pH2} text-lg mb-3 font-my-sans`}>
         Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
       </p>
 
