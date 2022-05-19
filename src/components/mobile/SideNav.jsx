b29ff174ca9a0fea5658e014e716d6d2b9518f90
jisena.btc <pjohmaths@gmail.com>
2022-05-19 11:01:07 +0300

    add content to side nav

--- src/components/mobile/SideNav.jsx
+++ src/components/mobile/SideNav.jsx
@@ -3,13 +3,18 @@ import React from 'react';
 export default function SideNav() {
   return (
     <div className='absolute top-full right-0 
-                  bg-cool-blue h-[80vh] w-3/4 
-                  border-t-cool-white border
+                  bg-cool-blue h-[20vh] w-3/4 border
                   rounded-tl-xl rounded-bl-xl'>
-      <ul>
-        <li>about</li>
-        <li>projects</li>
-        <li>contact</li>        
+      <ul className='flex flex-col mt-4 ml-12'>
+        <li className='border-cool-white border-b-2 mb-1'>
+          <h2 className='text-cool-white font-bold font-my-sans text-2xl'>about</h2>
+        </li>
+        <li className='border-cool-white border-b-2 mb-1'>
+          <h2 className='text-cool-white font-bold font-my-sans text-2xl'>projects</h2>
+        </li>
+        <li className='border-cool-white border-b-2 mb-1'>
+          <h2 className='text-cool-white font-bold font-my-sans text-2xl'>contact</h2>
+        </li>        
       </ul>
     </div>
   );
