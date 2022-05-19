51f93fdaa37ce2e2cf3b9e2d2a1102c7c9f6cfb6
jisena.btc <pjohmaths@gmail.com>
2022-05-19 19:42:57 +0300

    fixed back to the top button

--- src/components/mobile/BackToTheTop.jsx
+++ src/components/mobile/BackToTheTop.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {BiUpArrowAlt} from 'react-icons/bi';
+import { ThemeContext } from '../theme-context';
 
 export default function BackToTheTop({show}) {
+  const theme = useContext(ThemeContext);
   return (
     <a href='#top'>
-    <div className={`fixed ${show ? 'right-2': 'right-full'} top-[90%] rounded-full bg-cool-blue border-[1px] h-12 w-12`}>
-      <BiUpArrowAlt className='w-8 h-8 text-cool-white mx-auto mt-2'/>
+    <div className={`fixed ${show ? 'right-2': 'right-full'} top-[90%] rounded-full ${theme.iconbg} border-[1px] h-12 w-12`}>
+      <BiUpArrowAlt className={`w-8 h-8 ${theme.icon} mx-auto mt-2`}/>
     </div>
     </a>
   );
