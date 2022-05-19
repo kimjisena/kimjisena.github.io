9b364b060f506987b6ecd34fe3d5d4183e145227
jisena.btc <pjohmaths@gmail.com>
2022-05-19 12:04:48 +0300

    add theme toggler interactivity

--- src/components/mobile/ToggleTheme.jsx
+++ src/components/mobile/ToggleTheme.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import { MdNightlight } from 'react-icons/md';
 import { MdLightMode } from 'react-icons/md';
 
 export default function ToggleTheme() {
+  const [light, setLight] = useState(true);
+
   return (
-    <div className='border-[1px] border-cool-black col-span-1 w-20 mt-3 ml-2 flex flex-row justify-between h-9 rounded-full bg-cool-black'>
-        <div className='order-2'>
-            <MdLightMode className='text-cool-white' size='32px' />
+    <div className='border-[1px] border-cool-black 
+                    col-span-1 w-20 mt-3 ml-2 flex 
+                    flex-row justify-between h-9 
+                    rounded-full bg-cool-black'
+          onClick={() => setLight(!light)}>
+        <div className={`${light ? '' : 'order-2'}`}>
+            <MdNightlight className='text-[#c0876a]' size='32px' />
         </div>
         <div className='order-1'>
-            <MdNightlight className='text-[#c0876a]' size='32px' />
+            <MdLightMode className='text-cool-white' size='32px' />
         </div>
   </div>
   );
