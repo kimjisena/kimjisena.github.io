d227deb54f63ef49b6d9ed51a8bfbd9a7507e849
jisena.btc <pjohmaths@gmail.com>
2022-05-19 11:06:26 +0300

    use rem instead of vh/vw

--- src/components/mobile/SideNav.jsx
+++ src/components/mobile/SideNav.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function SideNav() {
   return (
     <div className='absolute top-full right-0 
-                  bg-cool-blue h-[20vh] w-3/4 border
-                  rounded-tl-xl rounded-bl-xl'>
-      <ul className='flex flex-col mt-4 ml-12'>
+                  bg-cool-blue w-3/4 border
+                  rounded-tl-xl rounded-bl-xl h-40'>
+      <ul className='flex flex-col mt-5 ml-12'>
         <li className='border-cool-white border-b-2 mb-1'>
           <h2 className='text-cool-white font-bold font-my-sans text-2xl'>about</h2>
         </li>
