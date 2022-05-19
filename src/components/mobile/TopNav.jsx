a46ca6231d796047b9d779110a4b21d1a7a0625a
jisena.btc <pjohmaths@gmail.com>
2022-05-19 12:10:47 +0300

    add open/close side nav interactivity

--- src/components/mobile/TopNav.jsx
+++ src/components/mobile/TopNav.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 import ToggleTheme from './ToggleTheme';
 import SideNav from './SideNav';
 import {VscMenu} from 'react-icons/vsc';
 import { VscChromeClose } from 'react-icons/vsc';
 
 export default function TopNav() {
+  const [sideNav, setSideNav] = useState(false);
   return (
     <header className='fixed top-0 w-full h-14 bg-cool-blue'>
       <div className='grid grid-cols-3 h-full'>
@@ -12,7 +14,8 @@ export default function TopNav() {
         <h1 className='text-center text-cool-white font-bold font-my-sans text-2xl align-middle mt-3 col-span-1'>{'<jisena />'}</h1>
         <div className='col-span-1 flex flex-row-reverse pr-4'>
           <div className='mt-3'>
-            <VscChromeClose className='text-cool-white' size='32px'/>
+            {sideNav ? <VscChromeClose className='text-cool-white' size='32px'/>
+                     : <VscMenu className='text-cool-white' size='32px'/>}
           </div>
         </div>
       </div>
