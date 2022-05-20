0a2ea172d329ec822031fcb00109d7f8ba74dade
jisena.btc <pjohmaths@gmail.com>
2022-05-20 07:04:28 +0300

    add a notification number

--- src/components/mobile/Notification.jsx
+++ src/components/mobile/Notification.jsx
@@ -7,6 +7,7 @@ export default function Notification({show}) {
   return (
     <div className={`fixed ${show ? 'right-full': 'right-2'} top-[90%] rounded-full ${theme.iconbg} h-12 w-12`}>
       <BsChatRightText className={`w-8 h-8 ${theme.icon} mx-auto mt-2`}/>
+      <p className='absolute -top-2 text-lg left-[25%] font-bold bg-red-500 w-6 h-6 text-center rounded-full'>1</p>
     </div>
   );
 }
