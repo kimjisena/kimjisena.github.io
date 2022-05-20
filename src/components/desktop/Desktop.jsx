605ebcf2fdcbee1273d7d1f73526785023472b2e
jisena.btc <pjohmaths@gmail.com>
2022-05-21 00:12:37 +0300

    add animated text to desktop version

--- src/components/desktop/Desktop.jsx
+++ src/components/desktop/Desktop.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import {FaSpinner} from 'react-icons/fa';
 
 export default function Desktop() {
   return (
     <div className='hidden sm:contents'>
-      <header className='fixed top-0'>
-        <h1>Navigation</h1>
-      </header>
       <div className='mx-6'>
-        <h1 className='mt-9 text-5xl text-cool-black font-my-sans'>
+        <h1 className={`mt-9 text-7xl text-cool-blue font-my-serif 
+                        shadow-xl shadow-cool-black
+                        font-bold animate-bounce text-center h-24`}>
           Desktop version coming soon!
         </h1>
+        <div className='fixed top-[70%] left-[50%]'>
+          <FaSpinner size={`60px`} className={`animate-spin text-dark-cool-blue`}/>
+        </div>
       </div>
     </div>
   );
