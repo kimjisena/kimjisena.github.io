869ddb538c38869cb57ef4cfe52fa38ecece5e2e
jisena.btc <pjohmaths@gmail.com>
2022-05-20 08:40:30 +0300

    cleaned up notification slider

--- src/components/mobile/Notification.jsx
+++ src/components/mobile/Notification.jsx
@@ -17,7 +17,7 @@ export default function Notification({show}) {
     <div className={`fixed ${show ? 'right-2': 'right-full'} top-[90%] rounded-full ${theme.iconbg} h-12 w-12`}
          onClick={()=> setNotify(!notify)}>
       <BsChatRightText className={`w-8 h-8 ${theme.icon} mx-auto mt-2`}/>
-      <p className='absolute -top-2 text-lg left-[25%] font-bold bg-red-500 w-6 h-6 text-center rounded-full'>1</p>
+      <p className={`absolute -top-2 text-lg left-[25%] font-bold bg-red-500 w-6 h-6 text-center rounded-full ${notify ? 'invisible': ''}`}>1</p>
       <NotificationSlider show={show} notify={notify}/>
     </div>
   );
