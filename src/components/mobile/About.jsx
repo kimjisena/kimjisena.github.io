d115fe84acd1fdcb0a81b23435c1f96e2a4df815
jisena.btc <pjohmaths@gmail.com>
2022-05-20 08:57:05 +0300

    cleaned up about section

--- src/components/mobile/About.jsx
+++ src/components/mobile/About.jsx
@@ -20,33 +20,21 @@ export default function About() {
       <TopImage />
       {/*section main content*/}
       <div className='col-span-full mt-2'>
-      <h1 id='about' className='invisible h-12'>about me</h1>
+        <h1 id='about' className='invisible h-12'>about me</h1>
         <h1 className={`text-4xl ${theme.H1} font-bold border-b-2 ${theme.h1Border} font-my-sans`}>about me</h1>
         <h2 className={`${theme.pH2} text-2xl font-bold font-my-sans`}>who am i</h2>
-          <Paragraph text={text} theme={theme.pH2} />
-          <Paragraph text={text} theme={theme.pH2} />
-          <Paragraph text={text} theme={theme.pH2} />
+        <Paragraph text={text} theme={theme.pH2} />
+        <Paragraph text={text} theme={theme.pH2} />
+        <Paragraph text={text} theme={theme.pH2} />
         <h2 className={`${theme.pH2} text-2xl font-bold font-my-sans`}>what do i</h2>
-        <p className={`${theme.pH2} text-lg font-my-sans`}>
-            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
-          </p>
-          <p className={`${theme.pH2} text-lg font-my-sans`}>
-            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
-          </p>
-          <p className={`${theme.pH2} text-lg font-my-sans`}>
-            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
-          </p>
-          <Skills />
+        <Paragraph text={text} theme={theme.pH2} />
+        <Paragraph text={text} theme={theme.pH2} />
+        <Paragraph text={text} theme={theme.pH2} />
+        <Skills />
         <h2 className={`${theme.pH2} text-2xl font-bold font-my-sans`}>what love i</h2>
-        <p className={`${theme.pH2} text-lg font-my-sans`}>
-            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
-          </p>
-          <p className={`${theme.pH2} text-lg font-my-sans`}>
-            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
-          </p>
-          <p className={`${theme.pH2} text-lg font-my-sans`}>
-            Ipsum est proident et ea non. Officia ut magna in deserunt consequat laborum fugiat sint. Ipsum labore enim irure sunt exercitation do sit qui quis.
-          </p>
+        <Paragraph text={text} theme={theme.pH2} />
+        <Paragraph text={text} theme={theme.pH2} />
+        <Paragraph text={text} theme={theme.pH2} />
       </div>
     </section>
     </>
