64143f436fe56a4bc9fa76ec683accce6b4e8ea4
jisena.btc <pjohmaths@gmail.com>
2022-05-20 18:16:21 +0300

    configured app for deployment to GitHub pages

--- src/components/dummy.js
+++
@@ -1,7 +0,0 @@
-export const para =
-                    `Quis veniam id excepteur excepteur aute aliquip eiusmod enim. 
-                    Cillum labore et consectetur deserunt est incididunt minim ad esse. 
-                    Duis voluptate ad pariatur esse nostrud mollit ullamco ullamco non incididunt. 
-                    Sunt adipisicing incididunt esse deserunt. Duis aute consectetur enim nisi proident. 
-                    Cupidatat laborum esse fugiat cillum pariatur sunt ex veniam dolor velit nisi id enim esse. 
-                    Labore velit excepteur pariatur deserunt do enim qui.`;
